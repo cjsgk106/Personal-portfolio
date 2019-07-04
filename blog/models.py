78a6df1e9e574ed8a9f9ca820c5e9f7ae3d6773b
Gerrard <cjsgk106@gmail.com>
2019-07-03 19:50:30 -0500

    Added jobs using Bootstrap!

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
 # Create your models here.
+# title
+# pub_date
+# body
+# image
+
+# Add the Blog app to the settings
+
+# Create a migration
+
+# Migrate
+
+# add to the admin
