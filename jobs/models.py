0c35cb28a9e3da213bdbc5fe6d62316c37a1e805
Gerrard <cjsgk106@gmail.com>
2019-12-29 08:25:01 -0600

    project items updated

--- jobs/models.py
+++ jobs/models.py
@@ -5,8 +5,9 @@ class Job(models.Model):
     title = models.CharField(max_length=50, blank=True, null=True)
     language = models.CharField(max_length=50, blank=True, null=True)
     pub_date = models.DateTimeField(default=datetime.now)
-    image = models.ImageField(upload_to='images/')
-    summary = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images/', blank=True)
+    summary = models.CharField(max_length=100)
+    url = models.URLField(max_length=200, blank=True)
 
     def __str__(self):
         return self.title
